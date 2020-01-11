2d8aac11bd8f1542228e1686f62d57ca491afc3a
abd <tooezabd@gmail.com>
2020-01-11 13:44:28 +0000

    Ep 2 . Got working for 1 env
    got a working Q learning algorithm . is able to learn in few thounds of epsoides and does render stuff

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
 import gym
 import numpy as np
+import math
 
 env = gym.make("MountainCar-v0")
 env.reset()
 
-print(env.observation_space.high)
-print(env.observation_space.low)
-print(env.action_space.n)
+# print(env.observation_space.high)
+# print(env.observation_space.low)
+# print(env.action_space.n)
+# parameters
+
+lr = 0.1
+discount = 0.95
+episode = 25000
+display = 2000
 
 state_size = [20] * len(env.observation_space.high)
 state_width = (env.observation_space.high - env.observation_space.low) / state_size
-print(state_width)
+# print(state_width)
 
 Q_table = np.random.uniform(low=-2, high=0, size=(state_size + [env.action_space.n]))
-print(Q_table.shape)
-finished = False
 
-while not finished:
-    action = 2
-    new_s, reward, finished, _ = env.step(action)
-    env.render()
+
+# print(Q_table.shape)
+
+
+def get_discrete(state):
+    state_discrete = (state - env.observation_space.low) / state_width
+    return tuple(state_discrete.astype(np.int))
+
+
+for ep in range(episode):
+    finished = False
+    # reseting enviroment and geting argmax of first q value
+    state_d = get_discrete(env.reset())
+
+    while not finished:
+        action = np.argmax(Q_table[state_d])
+        # doing the action
+        new_s, reward, finished, _ = env.step(action)
+
+        new_state_d = get_discrete(new_s)
+
+        # calcating next action and table
+        if not finished:
+
+            Max_future = np.max(Q_table[new_state_d])
+            Q_value_new = (1 - lr) * Q_table[state_d + (action,)] + lr * (reward + discount * Max_future)
+            Q_table[state_d + (action,)] = Q_value_new
+
+        elif new_s[0] >= env.goal_position:
+            print("won the game at ep = {}".format(ep))
+            Q_table[state_d + (action,)] = 0
+        state_d = new_state_d
+
+        if ep % display == 0:
+            print("current ep = {}".format(ep))
+            env.render()
+
 
 env.close()
 print("program is finished ")
