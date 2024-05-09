e46cf40ca4ab647c50e0ef2271ee1555b02c110d
abdul basit <abdul.basit@manchester.ac.uk>
2024-05-09 15:00:00 +0100

    Updated the display code to allow for plotting of every nth episode. This is done using the rgb_array render mode then saving the frames and after the episode create the animation using matplotlib.

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import gymnasium as gym
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from time import sleep
 
 
-env = gym.make("MountainCar-v0")
-env.reset()
-
-# print(env.observation_space.high)
-# print(env.observation_space.low)
-# print(env.action_space.n)
-# parameters
-
-lr = 0.1
-discount = 0.95
+lr = 0.1  # learning rate
+discount = 0.95  # discount factor
 episode = 25000
 display = 2000
 epsilon = 0.5  # chance we explore instead of trying optimal action
@@ -19,16 +14,22 @@ start_decay = 1  # which episode we start to decay
 stop_decay = episode // 2
 decay_amount = epsilon / (stop_decay - start_decay)
 
+env = gym.make("MountainCar-v0",render_mode = "rgb_array")
+env.reset()
 state_size = [20] * len(env.observation_space.high)
 state_width = (env.observation_space.high - env.observation_space.low) / state_size
-# print(state_width)
+
 
 Q_table = np.random.uniform(low=-2, high=0, size=(state_size + [env.action_space.n]))
 
+display_stack = []
 
-# print(Q_table.shape)
+def plot_ani(stack):
+    fig_stack = []
+    for img in stack:
+        fig_stack.append([plt.imshow(img,animated=True)])
 
-
+    return fig_stack
 def get_discrete(state):
     state_discrete = (state - env.observation_space.low) / state_width
     return tuple(state_discrete.astype(int))
@@ -45,7 +46,7 @@ for ep in range(episode):
         else:
             action = np.random.randint(low=0, high=env.action_space.n)
         # doing the action
-        new_s, reward, finished, compleat,_ = env.step(action)
+        new_s, reward, finished, compleat, _ = env.step(action)
 
         finished = compleat or finished
         new_state_d = get_discrete(new_s)
@@ -63,13 +64,21 @@ for ep in range(episode):
         state_d = new_state_d
 
         if ep % display == 0:
-            print("current ep = {}".format(ep))
-            env.render()
+            # print("current ep = {}".format(ep))
+            display_stack.append(env.render())
 
     if stop_decay > ep > start_decay and epsilon > 0:
         epsilon -= decay_amount
     elif epsilon < 0:
         epsilon = 0
+
+    if ep % display == 0:
+        fig = plt.figure()
+        frames = plot_ani(display_stack)
+        ani = animation.ArtistAnimation(fig=fig, artists=frames, interval=30)
+        plt.show()
+        sleep(0.1)
+        display_stack = []
 
 env.close()
 print("program is finished ")
